bdf7e5e5479c02fcf9bfc544c8b937e9d1de78b5
Oleg Grenrus <oleg.grenrus@iki.fi>
2016-02-11 15:00:26 +0200

    Refactor update-github-deps

--- src/StackYaml/Transformations/UpdateGithubDeps.hs
+++ src/StackYaml/Transformations/UpdateGithubDeps.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE OverloadedStrings #-}
 module StackYaml.Transformations.UpdateGithubDeps
   ( transformation
@@ -6,29 +7,37 @@ module StackYaml.Transformations.UpdateGithubDeps
 import Control.Applicative         (many, (<|>))
 import Control.Lens
 import Control.Monad.Catch         (throwM)
+import Control.Monad.IO.Class      (liftIO)
+import Control.Monad.Trans.Maybe   (MaybeT (..))
 import Data.Aeson.Lens
+import Data.Maybe                  (fromMaybe)
 import Data.Text                   (Text, pack)
 import Data.Yaml                   (Object, Value)
+import Network.HTTP.Client         (Manager, newManager)
+import Network.HTTP.Client.TLS     (tlsManagerSettings)
 import Text.Regex.Applicative.Text (match, psym)
 
 import qualified Data.Vector as V
 import qualified GitHub      as GH
 
 transformation :: Value -> IO Value
-transformation =
-    traverseOf (key "packages" . values . key "location" . _Object) updateGithubDep
+transformation value = do
+    manager <- newManager tlsManagerSettings
+    traverseOf (key "packages" . values . key "location" . _Object) (updateGithubDep manager) value
   where
-    updateGithubDep :: Object -> IO Object
-    updateGithubDep obj = case (obj ^? ix "git" . _String) >>= parseGithub of
-      Nothing -> pure obj
-      Just (owner, repo) -> do
-          res <- GH.executeRequest' $ GH.branchesForR owner repo $ Just 1
-          case res of
-              Left err       -> throwM err
-              Right branches -> case V.find ((== "master") . GH.branchName) branches of
-                  Nothing     -> pure obj
-                  Just branch -> let obj' = obj & ix "commit" . _String .~ GH.branchCommitSha (GH.branchCommit branch)
-                                 in pure obj'
+    updateGithubDep :: Manager -> Object -> IO Object
+    updateGithubDep mgr obj = fmap (fromMaybe obj) . runMaybeT $ do
+        (owner, repo) <- liftMaybe $ (obj ^? ix "git" . _String) >>= parseGithub
+        branches      <- liftIO    $ executeRequest mgr $ GH.branchesForR owner repo $ Just 1
+        branch        <- liftMaybe $ V.find ((== "master") . GH.branchName) branches
+        let obj'      =  obj & ix "commit" . _String .~ GH.branchCommitSha (GH.branchCommit branch)
+        pure obj'
+
+liftMaybe :: Applicative m => Maybe a -> MaybeT m a
+liftMaybe = MaybeT . pure
+
+executeRequest :: Manager -> GH.Request 'False a -> IO a
+executeRequest mgr req = either throwM pure =<< GH.executeRequestWithMgr' mgr req
 
 parseGithub :: Text -> Maybe (GH.Name GH.Owner, GH.Name GH.Repo)
 parseGithub = match githubRe
